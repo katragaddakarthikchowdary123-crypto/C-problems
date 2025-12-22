f652bb0a6b2c03d7492c6cb44e3ea10fcf5f48e6
katragaddakarthikchowdary123-crypto <katragaddakarthikchowdary123@gmail.com>
2025-12-22 21:42:34 +0530

    Add files via upload

---
+++ 21-12-25/k2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int age;
+    printf("enter the age");
+    scanf("%d",&age);
+
+if (age<=15)
+printf(" is a children");
+else
+printf("ia a  student");
+return 0;
+}
