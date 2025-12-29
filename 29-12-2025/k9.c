3da74e453a6593213a2b497377706925daea113b
katragaddakarthikchowdary123-crypto <katragaddakarthikchowdary123@gmail.com>
2025-12-29 21:42:59 +0530

    Add files via upload

---
+++ 29-12-2025/k9.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int marks;
+    scanf("%d", &marks);
+
+    if (marks >= 80)
+        printf("Grade A");
+    else if (marks >= 60)
+        printf("Grade B");
+    else
+        printf("Grade C");
+
+    return 0;
+}
