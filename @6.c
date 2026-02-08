ffb84fb0c23769dd0cb7869da92bffc62b4ec699
katragaddakarthikchowdary123-crypto <katragaddakarthikchowdary123@gmail.com>
2026-02-08 21:48:06 +0530

    Add files via upload

---
+++ @6.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    float num=3.4;
+    printf("%f",num);
+    
+    
+    return 0;
+}
---
+++ @7.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    float num=7.25;
+    printf("%f",num);
+    
+    
+    return 0;
+}
