084dd3567b2c1318d876525ed7264667718ce521
katragaddakarthikchowdary123-crypto <katragaddakarthikchowdary123@gmail.com>
2026-02-19 20:32:39 +0530

    Add files via upload

--- switch case p34.c
+++ switch case p34.c
@@ -2,18 +2,12 @@
 int main() {
     int hours;
     float salary;
-
     scanf("%d %f", &hours, &salary);
-
-    if(hours > 40){
-        salary += (hours - 40) * 200;
+     if(hours > 40){
+        salary += (hours - 40)*200;
     }
     else{
-
-    
-
-    printf("Total Salary %.2f", salary);
+    printf("total salary %.2f",salary);
     }
-
-    return 0;
+   return 0;
 }
